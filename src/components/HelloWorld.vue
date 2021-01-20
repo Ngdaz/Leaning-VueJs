c12aae984d85535eb74dda8ea35200740e0e673a
portgasdax99 <ngdaaz@gmail.com>
2021-01-20 16:38:43 +0700

    how :value work

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,79 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="form-input">
+    <!-- {{isAvailable}}
+    <el-switch v-model="isAvailable"></el-switch>
+     <p v-if="isAvailable == true">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sit cumque dicta! Maiores doloremque inventore beatae veniam magnam tempore nam aliquam fuga ratione enim dolores quidem, et reprehenderit minus aut!</p>
+    <p v-else>Truong Hop Nguoc Lai</p> -->
+    <!-- switch 1 -->
+    <!-- <el-switch
+      v-model="value"
+      active-color="#13ce66"
+      inactive-color="#ff4949">
+    </el-switch>
+    <p v-if="value == true">bat but</p>
+    <p v-else> tat nut</p> -->
+
+    <!-- <input type="text" v-model="text"> -->
+    
+    <!-- username:
+    <input
+      class="username"
+      type="text"
+      :value="userName"
+      @input="onChangeUserName"
+      placeholder="enter your username"
+    />
+    password
+    <input
+      class="pass"
+      type="text"
+      :value="password"
+      @input="onChangePass"
+      placeholder="enter you your password"
+    />
+    username is {{ userName }}
+    <br>
+    password is {{password}}
+    {{value}} -->
+
+    <!-- su dung v-model  -->
+
+  <!-- <input  class="username" type="text" v-model="userName" placeholder="enter your username">
+  <input  class="password" type="text" v-model="password" placeholder="enter your password">
+    {{userName}}
+    {{password}} -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      value:"",
+      userName:"",
+      password:""
+    };
+  },
+  methods: {
+    // onChangeUserName(test) {
+    //   console.log(test.target);
+    //   this.userName = test.target.value;
+    //   console.log("username:",this.userName);
+    // },
+    // onChangePass(test) {
+    //   this.password = test.target.value;
+    // },
+  },
+  mounted() {},
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.form-input {
+  display: flex;
+  flex-direction: column;
+  width: 16rem;
+  margin: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
