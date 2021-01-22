57057351d739966ddfb1f9661308667083adb8da
portgasdax99 <ngdaaz@gmail.com>
2021-01-22 08:46:52 +0700

    create a form and alert the result, using v-model

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
-    <p>{{counter}}</p>
+    <!-- <div  v-for=" item in forms"> -->
+    <!-- form input -->
+    <p v-if="form.status">ban da tao tai khoan thanh cong</p>
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label=" Your name">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="Age">
+        <el-input v-model="form.age"></el-input>
+      </el-form-item>
+      <el-form-item label="Adress">
+        <el-input v-model="form.address"></el-input>
+      </el-form-item>
+    </el-form>
+    <!-- submit -->
+    <el-button type="primary" @click="onSubmit">Create</el-button>
+
+    <!-- Table -->
+    <!-- <el-button type="button" @click="dialogTableVisible = true"
+      >Open record</el-button
+    > -->
+
+    <el-dialog title="Reord" :visible.sync="dialogTableVisible">
+      <el-table :data="[form]">
+        <el-table-column prop="name" label="Name ne"></el-table-column>
+        <el-table-column prop="age" label="Age ne"></el-table-column>
+        <el-table-column prop="address" label="Address ne"></el-table-column>
+      </el-table>
+      <el-button type="primary" @click="onClose">Ok</el-button>
+    </el-dialog>
   </div>
 </template>
 
@@ -8,22 +37,30 @@
 export default {
   data() {
     return {
-      counter:'testf  ',
+      form: [
+        {
+          name: "",
+          age: "",
+          address: "",
+          status: false,
+        },
+      ],
+      dialogTableVisible: false,
+
+      formLabelWidth: "120px",
     };
   },
   methods: {
-   
+    onSubmit: function () {
+      this.dialogTableVisible = true;
+    },
+    onClose: function () {
+      this.form.status = true;
+      this.dialogTableVisible = false;
+    },
   },
-  beforeCreate() {alert('beforeCreate')},
-  created(){alert('created')},
-  beforeMount(){alert('beforeMount')},
-  mounted() {alert('mounted')},
-  beforeUpdate() {alert('beforeUpdate')},
-  updated(){alert('updated')}
 };
 </script>
 
 <style>
-
-
 </style>
